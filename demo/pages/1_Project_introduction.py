68dd1c5468e3fb03c98f2e85e2091b445186cfc9
Maxime VANPEENE <vanpeene.maxime@gmail.com>
2022-06-12 13:52:00 +0200

    streamlit premier jet

---
+++ demo/pages/1_Project_introduction.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write('# Project introduction ')
+st.write('The financial and banking world is one of the sectors that generates the most data and is most\
+ often freely accessible. The portfolio allocation domain is particularly interesting in the sense that it is abundant in data and \
+    can be systematized.')
+
+st.write('The objective of this project is to create a portfolio allocation model that adapts its strategy online.')
