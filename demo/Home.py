ecd1f7a225f08139f24226c8dfd75fc12f786974
Maxime VANPEENE <vanpeene.maxime@gmail.com>
2022-06-12 14:28:08 +0200

    commit parties 1 et 2

--- demo/Home.py
+++ demo/Home.py
@@ -5,5 +5,5 @@ st.write('### Online Portfolio Allocation benchmarking with Python',)
 st.write(' *Data Scientist promo Sep21 - Sandra CHARLERY, Maxime VANPEENE*')
 
 from PIL import Image
-image = Image.open('intro_image_finance.jpeg')
+image = Image.open('./img/intro_image_finance.jpeg')
 st.image(image)
--- demo/pages/2_Dataset.py
+++ demo/pages/2_Dataset.py
@@ -1,3 +1,26 @@
 import streamlit as st
+import pandas as pd
 
-st.write('The dataset is made of the 100 largest capitalizations in the NYSE stock market')
+st.write('# Dataset')
+
+st.write('The dataset is made of the 100 largest capitalizations in the NYSE stock market at the time of the project')
+st.write('These values are easy to buy and sell and can be used in a dynamic portfolio allocation strategy.')
+
+
+#import du csv avec 500 valeurs du SP500
+#colonnes (ticker,name,sector,subsector)
+sectors=pd.read_csv('./csv/sp500_sectors.csv',encoding='cp1252')
+sectors['ticker']=sectors['ticker'].map(lambda x:x.lower())
+sectors.index = sectors.index.map(str)
+#on filtre sur les 100 actions qui nous intéressent et on met le ticker comme index
+data100=pd.read_csv('./csv/data100_09042022.csv',index_col=0,header=0,parse_dates=True)
+
+sectors_100=sectors[sectors['ticker'].isin(data100.columns.to_list())]
+sectors_100=sectors_100.set_index('ticker')
+
+st.dataframe(sectors_100)
+
+st.write('The considered value is the Adjusted Close, which was taken between the 07th of December 2018 and the 08th of April 2022.')
+st.write('Below are the 10 first lines of the dataset.')
+
+st.dataframe(data100.head(10))
