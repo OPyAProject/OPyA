90524d380fc22e6a93749f360fc67c7e2e8e6513
santhuria <93120115+santhuria@users.noreply.github.com>
2022-06-14 16:58:52 +0200

    Benchmarking update

--- demo/pages/5_Available_strategies.py
+++ demo/pages/5_Available_strategies.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from prophet import Prophet
+from prophet.plot import plot_cross_validation_metric 
+from prophet.diagnostics import performance_metrics
+from prophet.diagnostics import cross_validation
+import pandas as pd
+import matplotlib.pyplot as plt
+#warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+data100=pd.read_csv('./csv/data100_09042022.csv',index_col=0,header=0,parse_dates=True)
 
 st.write('# Toward Modern Portfolio Strategies')
 
@@ -33,4 +44,79 @@ elif add_select_detail_strategy == 'Allocation Strategy':
     image2 = Image.open('./img/13_ef_scatter.PNG')
     st.image(image2)
 elif add_select_detail_strategy == 'E(R) Estimation Method':
-    st.write("You selected E(R) Estimation Method.")
+    st.write('## E(R) Estimation Method')
+    st.write("### Historical Mean")
+    st.write('The mean historical return of an asset is its past rate of return and performance.')
+    image = Image.open('./img/14_historical_mean_formula.PNG')
+    st.image(image)
+    st.write("### EMA : Exponential Moving Average")  
+    st.write('The EMA is a kind of moving average that gives more importance to recent data.')
+    image = Image.open('./img/15_ema_formula.PNG')
+    st.image(image)
+    st.write("### CAPM : Capital Asset Pricing Model")
+    image = Image.open('./img/capm_formula.PNG')
+    st.image(image)
+    st.write("### Future Prediction : Facebook Prophet Model")
+    image = Image.open('./img/prophet_formula.PNG')
+    st.image(image)
+    st.write("#### Sample output")
+    ticker = st.selectbox('Select a share :',  (data100.columns.tolist()))
+
+    ticker_list_for_forecast = [ticker]
+    data_to_forecast = data100
+    #we forecast Ajd Close Prices for the example
+
+    ticker_models = {}
+    ticker_cross_validations = {}
+    ticker_performances ={}
+    ticker_forecasts = {}
+    forecast_dataset = pd.DataFrame()
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        for i,ticker in enumerate(ticker_list_for_forecast):
+            ticker_data=data_to_forecast[ticker].to_frame()
+            ticker_data.reset_index(inplace=True)
+            
+            ticker_data.columns=["ds","y"]
+
+            nb_obs = len(ticker_data)
+            
+            train_data, test_data = ticker_data[:int(nb_obs*0.7)], ticker_data[int(nb_obs*0.3):]
+            
+            # Creation of Prophet model with default parameters, but adding US Country Holidays
+            ticker_models[ticker] = Prophet()
+            ticker_models[ticker].add_country_holidays(country_name='US')
+            ticker_models[ticker].fit(train_data)
+            #future = ticker_models[ticker].make_future_dataframe(periods=int(nb_obs*0.1))
+            #forecast = ticker_models[ticker].predict(future)
+            
+            #applying predict to the last 30% dates and get the 70% fitted in sample datas in a single dataframe
+            ticker_forecast = ticker_models[ticker].predict(ticker_data)
+            ticker_forecasts[ticker] = ticker_forecast
+            
+            #we save all forecast in a single dataframe for all stocks selected
+            if (i==0):
+                forecast_dataset['ds'] = ticker_forecast['ds']
+            forecast_dataset[ticker] = ticker_forecast['yhat']
+            
+            # Display graphic of Actual vs Predicted values
+            fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15, 5))
+            
+            fig.suptitle("Actual Vs Forecast for " + ticker)
+            #using Prophet plotting
+            ticker_models[ticker].plot(ticker_forecast,ax=ax1)
+
+            #using standard plotting
+            ticker_data.plot(x='ds',y='y',ax=ax2)
+            ticker_forecast.plot(x='ds',y='yhat', ax=ax2)
+            st.pyplot(fig)
+
+            st.write('#### Prophet forecast componnets')
+            ticker_models[ticker].plot_components(ticker_forecast)
+            st.write(plt.gcf())
+            if(ticker == 'googl'):
+                ticker_cross_validations[ticker] = cross_validation(ticker_models[ticker], period='30 days', horizon = '30 days')
+                st.write('#### Prophet cross validation and metrics')
+                plot_cross_validation_metric(ticker_cross_validations[ticker], metric='rmse')
+                st.write(plt.gcf())
---
+++ demo/pages/6_Benchmarking.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.write("# Benchmarking method")
+
+st.write("## Benchmark")
+st.write("We use S&P 500 as the Reference to compare performances of the different strategies implemented in the project.")
+st.write("The S&P 500 is the most representative index of the American stock market because it is made up of \
+    a greater number of companies and its value takes into account of the market capitalization of the companies included in the index.")
+
+from PIL import Image
+image = Image.open('./img/benchmark.PNG')
+st.image(image)
+
+st.write("# Prophet Model Training")
+st.write("We decided to train and save Prophet Model on each stock for about the first 2 years of the dataset, until December 31, 2020.")
+st.write("Then, the forecasting period matches the backtesting period.")
+
+st.write("# Backtesting period")
+st.write("The backtesting period goes from January 01, 2021 to April 08, 2022.")
+st.write("For dynamic allocation, each strategy is run monthly.")
+
+st.write("# Backtesting tools")
+st.write("We use BT library as the framework to run the different strategies, and PyPortfolioOpt mainly for CAPM and EMA calculations.")
+
+code = '''\
+
+#Custom Algo
+class OPAMarkowitz(bt.Algo):
+    def __init__(self, ...)
+    def __call__(self, target):
+        selected = target.temp["selected"]
+        ...
+        target.temp["weights"] = tw.dropna()
+        return True    
+
+#Strategy definition
+sOPAMarkowitzML = bt.Strategy('sOPAMarkowitzML', 
+    [
+    bt.algos.RunAfterDate(startDateForStaticStrategy),
+    bt.algos.RunMonthly(),
+    bt.algos.SelectHasData(),
+    OPAMarkowitz(er_estimation_method=ER_ESTIMATION_METHOD.PROPHET,
+                 rf=RISK_FREE_RATE),
+    bt.algos.Rebalance()
+    ]
+    )
+#Data feeding
+testOPAMarkowitzML= bt.Backtest(sOPAMarkowitzML, data)
+
+#Run
+resML = bt.run(testBenchmarkSP500,
+               testOPAMarkowitzMLOnce,
+               testOPAMarkowitzML)
+#View result
+res.plot()
+
+#View stats
+res.display()
+'''
+st.code(code)
+image = Image.open('./img/res_display.PNG')
+st.image(image)
+st.write('# Benchmarking Results')
+st.sidebar.write('# Results')
+select_mode = st.sidebar.selectbox('Select Mode :',  ('Static vs Dynamic','All Dynamic'))
+if select_mode == 'Static vs Dynamic':
+    select_strategy = st.sidebar.selectbox('Select Strategy :',  ('Markowitz','Risk Parity', 'Momentum'))
+    if select_strategy == 'Markowitz':
+        select_estimation_method = st.sidebar.selectbox('Select E(R) Estimation method:',  ('Historical Mean','EMA', 'CAPM',  'Forecast'))
+        if select_estimation_method == 'Historical Mean':
+            image = Image.open('./img/markowitz_mean.png')
+            st.image(image)
+        elif select_estimation_method == 'EMA':
+            image = Image.open('./img/markowitz_ema.png')
+            st.image(image)
+        elif select_estimation_method == 'CAPM':
+            image = Image.open('./img/markowitz_capm.png')
+            st.image(image)
+        elif select_estimation_method == 'Forecast':
+            image = Image.open('./img/markowitz_ml.png')
+            st.image(image)
+    elif select_strategy == 'Risk Parity':
+        image = Image.open('./img/risk_parity_3M.png')
+        st.image(image)
+    elif select_strategy == 'Momentum':
+        image = Image.open('./img/momentum_3M.png')
+        st.image(image)
+
+
+if select_mode == 'All Dynamic':
+    image = Image.open('./img/opa_all.png')
+    st.image(image)
+    st.write('#### Sharpe Ratio Indicator')
+    image = Image.open('./img/opa_sharpe.png')
+    st.image(image)
