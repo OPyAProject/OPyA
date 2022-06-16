e91e615e90b9887d526d6f5b774e1e4696af195c
santhuria <93120115+santhuria@users.noreply.github.com>
2022-06-16 12:50:27 +0200

    datascientest logo

--- demo/Home.py
+++ demo/Home.py
@@ -1,9 +1,13 @@
 import streamlit as st
+from PIL import Image
+
+image = Image.open('./img/data_scientest_logo.png')
+st.image(image)
 
 st.write('# OPyA project')
 st.write('### Online Portfolio Allocation benchmarking with Python',)
 st.write(' *Data Scientist promo Sep21 - Sandra CHARLERY, Maxime VANPEENE*')
 
-from PIL import Image
+
 image = Image.open('./img/0_intro_image_finance.jpeg')
 st.image(image)
